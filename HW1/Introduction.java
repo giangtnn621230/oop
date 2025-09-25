d76fc63ad3b343aad2e56ef59b3ac2ca7ab16021
giang5201232 <24020112@vnu.edu.vn>
2025-09-25 20:35:06 +0700

    push Introduction

---
+++ HW1/Introduction.java
@@ -0,0 +1,14 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Họ và tên: Nguyễn Ngọc Trường Giang");
+        System.out.println("Mã sinh viên: 24020112");
+        System.out.println("Lớp: K69I-IT4");
+        System.out.println("Github Username: giangtnn621230");
+        System.out.println("Email: 24020112@vnu.edu.vn");
+        for (int i = 9; i >= 1; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i +" bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
